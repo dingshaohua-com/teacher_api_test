78fbd72522961e15c899a77e4566343b26db915a
丁少华 <960423114@qq.com>
2026-01-16 02:29:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 
 from dotenv import load_dotenv
-
 from utils.common import save_as_md
-
 load_dotenv('.env.test2')
 from service.auth import login
 from service.user import get_teacher_detail
-from mdutils.mdutils import MdUtils
 
 login()
 res=teacher_detail=get_teacher_detail()
